ee4d1acc201c128b7cbc4ef2d70844978b3d6c78
satollo <satollo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-04-11 13:25:04 +0000

    git-svn-id: https://plugins.svn.wordpress.org/hyper-cache/trunk@39729 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- advanced-cache.php
+++ advanced-cache.php
@@ -8,8 +8,8 @@ if (!$hyper_cache_enabled) return;
 if ($_SERVER["REQUEST_METHOD"] == 'POST') return;
 
 // Do not use or cache pages when a wordpress user is logged on
-foreach ($_COOKIE as $n=>$v) 
-{ 
+foreach ($_COOKIE as $n=>$v)
+{
     if (substr($n, 0, 13) == 'wordpressuser') return;
 }
 
@@ -26,10 +26,10 @@ if (strpos($hyper_uri, '/download/') !== false) return;
 
 // Remove the anchor
 $x = strpos($hyper_uri, '#');
-if ($x !== false) $hyper_uri = substr($hyper_uri, 0, $x); 
+if ($x !== false) $hyper_uri = substr($hyper_uri, 0, $x);
 
 
-    
+
 $hyper_cache_name = md5($hyper_uri);
 $hyper_file = ABSPATH . 'wp-content/hyper-cache/' . $hyper_cache_name . '.dat';
 
@@ -37,11 +37,11 @@ if (file_exists($hyper_file))
 {
     $hyper_data = unserialize(file_get_contents($hyper_file));
     if ($hyper_cache_timeout == null) $hyper_cache_timeout = 60;
-    if ($hyper_data != null && ($hyper_data['time'] > time()-($hyper_cache_timeout*60)) && $hyper_data['html'] != '') 
+    if ($hyper_data != null && ($hyper_data['time'] > time()-($hyper_cache_timeout*60)) && $hyper_data['html'] != '')
     {
         header('Content-Type: text/html;charset=UTF-8');
         echo $hyper_data['html'];
-        echo '<!-- -->';
+        //echo '<!-- -->';
         flush();
         die();
     }
@@ -50,21 +50,20 @@ if (file_exists($hyper_file))
 ob_start('hyper_cache_callback');
 
 
-function hyper_cache_callback($buffer)
-{
-global $hyper_file;
-  //$name = md5($_SERVER['REQUEST_URI']);
-  
-  $data['uri'] = $_SERVER['REQUEST_URI'];
-  $data['referer'] = $_SERVER['HTTP_REFERER'];
-  $data['time'] = time();
-  $data['html'] = $buffer;
-  
-  $file = fopen($hyper_file, 'w');
-  fwrite($file, serialize($data));
-  fclose($file);
-  
-  return $buffer;
+function hyper_cache_callback($buffer) {
+    global $hyper_file;
+    //$name = md5($_SERVER['REQUEST_URI']);
+
+    $data['uri'] = $_SERVER['REQUEST_URI'];
+    $data['referer'] = $_SERVER['HTTP_REFERER'];
+    $data['time'] = time();
+    $data['html'] = $buffer;
+
+    $file = fopen($hyper_file, 'w');
+    fwrite($file, serialize($data));
+    fclose($file);
+
+    return $buffer;
 }
 
 ?>
