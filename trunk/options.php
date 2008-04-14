178a725745e2e221e6a4a0baebfb6d8c418f0108
satollo <satollo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-04-14 13:13:21 +0000

    git-svn-id: https://plugins.svn.wordpress.org/hyper-cache@40620 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- trunk/options.php
+++ trunk/options.php
@@ -3,10 +3,16 @@ if (function_exists('load_plugin_textdomain')) {
     load_plugin_textdomain('hyper-cache', 'wp-content/plugins/hyper-cache');
 }
 
-function hyper_request($name, $default=null) {
-    if (!isset($_REQUEST[$name])) return $default;
-    if (get_magic_quotes_gpc()) return hyper_stripslashes($_REQUEST[$name]);
-    else return $_REQUEST[$name];
+function hyper_request( $name, $default=null ) {
+    if ( !isset($_POST[$name]) ) {
+    	return $default;
+    }
+    if ( get_magic_quotes_gpc() ) {
+    	return hyper_stripslashes($_POST[$name]);
+    }
+    else {
+    	return $_POST[$name];
+    }
 }
 
 function hyper_stripslashes($value) {
@@ -16,23 +22,23 @@ function hyper_stripslashes($value) {
 
 function hyper_field_checkbox($name, $label='', $tips='', $attrs='') {
     global $options;
-    echo '<tr><td class="label">';
-    echo '<label for="options[' . $name . ']">' . $label . '</label></td>';
-    echo '<td><input type="checkbox" ' . $attrs . ' name="options[' . $name . ']" value="1" ' .
-    ($options[$name]!= null?'checked':'') . '/>';
+    
+    echo '<th scope="row">';
+    echo '<label for="options[' . $name . ']">' . $label . '</label></th>';
+    echo '<td><input type="checkbox" ' . $attrs . ' name="options[' . $name . ']" value="1" ' . ($options[$name]!= null?'checked':'') . '/>';
     echo ' ' . $tips;
-    echo '</td></tr>';
+    echo '</td>';
 }
 
 function hyper_field_text($name, $label='', $tips='', $attrs='') {
     global $options;
     if (strpos($attrs, 'size') === false) $attrs .= 'size="30"';
-    echo '<tr><td class="label">';
-    echo '<label for="options[' . $name . ']">' . $label . '</label></td>';
+    echo '<th scope="row">';
+    echo '<label for="options[' . $name . ']">' . $label . '</label></th>';
     echo '<td><input type="text" ' . $attrs . ' name="options[' . $name . ']" value="' .
     htmlspecialchars($options[$name]) . '"/>';
     echo ' ' . $tips;
-    echo '</td></tr>';
+    echo '</td>';
 }
 
 function hyper_field_textarea($name, $label='', $tips='', $attrs='') {
@@ -41,12 +47,16 @@ function hyper_field_textarea($name, $label='', $tips='', $attrs='') {
     if (strpos($attrs, 'cols') === false) $attrs .= 'cols="70"';
     if (strpos($attrs, 'rows') === false) $attrs .= 'rows="5"';
 
-    echo '<tr><td class="label">';
-    echo '<label for="options[' . $name . ']">' . $label . '</label></td>';
+    echo '<th scope="row">';
+    echo '<label for="options[' . $name . ']">' . $label . '</label></th>';
     echo '<td><textarea wrap="off" ' . $attrs . ' name="options[' . $name . ']">' .
     htmlspecialchars($options[$name]) . '</textarea>';
-    echo '<br /> ' . $tips;
-    echo '</td></tr>';
+    echo '<br />' . $tips;
+    echo '</td>';
+}
+
+if (isset($_POST['clear'])) {
+    hyper_cache_invalidate();
 }
 
 if (isset($_POST['save'])) {
@@ -54,13 +64,14 @@ if (isset($_POST['save'])) {
     update_option('hyper', $options);
 
     // Write the configuration file
-
-    if (!file_exists(ABSPATH . '/wp-content/hyper-cache'))
-    {
+    if (!file_exists(ABSPATH . '/wp-content/hyper-cache')) {
         mkdir(ABSPATH . '/wp-content/hyper-cache', 0766);
     }
 
-    if (!$options['timeout'] || !is_numeric($options['timeout'])) $options['timeout'] = 60;
+    if (!$options['timeout'] || !is_numeric($options['timeout'])) {
+    	$options['timeout'] = 60;
+    }
+    
     $buffer = "<?php\n";
     $buffer .= '$hyper_cache_enabled = ' . ($options['cache']?'true':'false') . ";\n";
     $buffer .= '$hyper_cache_timeout = ' . $options['timeout'] . ";\n";
@@ -72,38 +83,49 @@ if (isset($_POST['save'])) {
     // Write the advanced-cache.php (so we grant it's the correct version)
     $buffer = file_get_contents(dirname(__FILE__) . '/advanced-cache.php');
     $file = fopen(ABSPATH . 'wp-content/advanced-cache.php', 'w');
+    
     fwrite($file, $buffer);
     fclose($file);
-}
-else
-{
+} else {
     $options = get_option('hyper');
-    if (!$options['timeout']) $options['timeout'] = 60;
+    if (!$options['timeout']) {
+    	$options['timeout'] = 60;
+    }
 }
 ?>
-<style type="text/css">
-    td.label{width: 150px;vertical-align: top;font-weight: bold;text-align: right;}
-    td textarea {font-family: monospace;font-size: 11px;}
-</style>
 <div class="wrap">
     <form method="post">
-        <h2><?php _e('Hyper Cache', 'hyper-cache'); ?></h2>
+        <h2>Hyper Cache</h2>
+        
         <?php
-        if (!defined('WP_CACHE')) {
-            $pre = __("<pre>define('WP_CACHE', true);</pre>", 'hyper-cache');
-            echo '<p>';
-            printf(__('The WordPress caching system is not enabled. Please add the line: %s in your wp-config.php file. Thank you.', 'hyper-cache'), $pre);
-            echo '</p>';
+        if (!defined('WP_CACHE') ) {
+            echo '<div class="alert error" style="margin-top:10px;"><p>';
+            _e('not_enabled', 'hyper-cache');
+            echo "<pre>define('WP_CACHE', true);</pre>";
+            echo '</p></div>';
         }
         ?>
 
-        <h3><?php _e('Configuration', 'hyper-cache'); ?></h3>
-        <table>
-        <?php hyper_field_checkbox('cache', __('Cache active?', 'hyper-cache')); ?>
-        <?php hyper_field_text('timeout', __('Single page cached expire after', 'hyper-cache'), __('(minutes!)', 'hyper-cache')); ?>
+        <h3><?php _e('configuration', 'hyper-cache'); ?></h3>
+        <table class="form-table">
+			<tr valign="top">
+        		<?php hyper_field_checkbox('cache', __('active', 'hyper-cache')); ?>
+        	</tr>
+        	<tr valign="top">
+       			<?php hyper_field_text('timeout', __('expire', 'hyper-cache'), __('minutes', 'hyper-cache'), 'size="5"'); ?>
+       		</tr>
         </table>
-
+        
+        <table class="form-table">
+			<tr valign="top">
+        		<th scope="row"><?php _e('count', 'hyper-cache'); ?></th>
+                <td><?php echo hyper_count(); ?></td>
+        	</tr>
+        </table>        
+        
         <p>
-            <input class="button" type="submit" name="save" value="<?php _e('Save', 'hyper-cache'); ?>"></p>
+            <input class="button" type="submit" name="save" value="<?php _e('save', 'hyper-cache'); ?>">  
+            <input class="button" type="submit" name="clear" value="<?php _e('clear', 'hyper-cache'); ?>">
+        </p>      
     </form>
 </div>
--- trunk/plugin.php
+++ trunk/plugin.php
@@ -3,13 +3,14 @@
 Plugin Name: Hyper Cache
 Plugin URI: http://www.satollo.com/english/wordpress/hyper-cache
 Description: Hyper Cache is an extremely aggressive cache for WordPress.
-Version: 1.0.3
+Version: 1.0.5
 Author: Satollo
 Author URI: http://www.satollo.com
 Disclaimer: Use at your own risk. No warranty expressed or implied is provided.
-*/
 
-/*	Copyright 2008  Satollo  (email : satollo@gmail.com)
+---
+Copyright 2008  Satollo  (email : satollo@gmail.com)
+---
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -24,6 +25,21 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+---
+Changelog
+---
+Version 1.0.5
+    - Add italian translation
+    - Add the "clear the cache" button
+    - Add the cache page count
+
+Version 1.0.4
+    - Thank you to Amaury Balmer for this version
+	- Add french translation
+	- Improve options with WordPress 2.5
+	- Fix bug with WP 2.5 cookies
+	- Minor changes
 */
 
 $hyper_options = get_option('hyper');
@@ -36,66 +52,72 @@ function hyper_activate() {
 
 add_action('deactivate_hyper-cache/plugin.php', 'hyper_deactivate');
 function hyper_deactivate() {
-    unlink(ABSPATH . 'wp-content/advanced-cache.php');
-    unlink(ABSPATH . 'wp-content/hyper-cache-config.php');
+	delete_option('hyper');
 
-    $path = ABSPATH . 'wp-content/' . time();
-    rename(ABSPATH . 'wp-content/hyper-cache', $path);
+	unlink(ABSPATH . 'wp-content/advanced-cache.php');
+	unlink(ABSPATH . 'wp-content/hyper-cache-config.php');
 
-    if ($handle = opendir($path))
-    {
-        while ($file = readdir($handle))
-        {
-            if ($file != '.' && $file != '..')
-            {
-                unlink($path . '/' . $file);
-            }
-        }
-        closedir($handle);
-        rmdir($path);
-    }
+	$path = ABSPATH . 'wp-content/' . time();
+	rename(ABSPATH . 'wp-content/hyper-cache', $path);
+
+	hyper_delete_path( $path );
 }
 
 add_action('admin_head', 'hyper_admin_head');
 function hyper_admin_head() {
-    add_options_page('Hyper Cache', 'Hyper Cache', 'manage_options', 'hyper-cache/options.php');
+	add_options_page('Hyper Cache', 'Hyper Cache', 'manage_options', 'hyper-cache/options.php');
 }
 
-function hyper_cache_invalidate()
-{
-    $path = ABSPATH . 'wp-content/' . time();
-    rename(ABSPATH . 'wp-content/hyper-cache', $path);
-    mkdir(ABSPATH . 'wp-content/hyper-cache', 0766);
+function hyper_cache_invalidate() {
+	$path = ABSPATH . 'wp-content/' . time();
+	rename(ABSPATH . 'wp-content/hyper-cache', $path);
+	mkdir(ABSPATH . 'wp-content/hyper-cache', 0766);
 
-    if ($handle = opendir($path))
-    {
-        while ($file = readdir($handle))
-        {
-            if ($file != '.' && $file != '..')
-            {
-                unlink($path . '/' . $file);
-            }
-        }
-        closedir($handle);
-        rmdir($path);
-    }
+	hyper_delete_path( $path );
 }
 
-if ($hyper_options['cache'])
-{
-    add_action('publish_post', 'hyper_cache_invalidate', 0);
-    add_action('edit_post', 'hyper_cache_invalidate', 0);
-    add_action('delete_post', 'hyper_cache_invalidate', 0);
-    add_action('publish_phone', 'hyper_cache_invalidate', 0);
-    // Coment ID is received
-    add_action('trackback_post', 'hyper_cache_invalidate', 0);
-    add_action('pingback_post', 'hyper_cache_invalidate', 0);
-    add_action('comment_post', 'hyper_cache_invalidate', 0);
-    add_action('edit_comment', 'hyper_cache_invalidate', 0);
-    add_action('wp_set_comment_status', 'hyper_cache_invalidate', 0);
-    // No post_id is available
-    add_action('delete_comment', 'hyper_cache_invalidate', 0);
-    add_action('switch_theme', 'hyper_cache_invalidate', 0);
+function hyper_delete_path( $path = '' ) {
+	if ($handle = opendir($path)) {
+		while ($file = readdir($handle)) {
+			if ($file != '.' && $file != '..') {
+				unlink($path . '/' . $file);
+			}
+		}
+		closedir($handle);
+		rmdir($path);
+	}
 }
 
+function hyper_count() {
+    $count = 0;
+	if ($handle = opendir(ABSPATH . 'wp-content/hyper-cache')) {
+		while ($file = readdir($handle)) {
+			if ($file != '.' && $file != '..') {
+				$count++;
+			}
+		}
+		closedir($handle);
+	}
+    return $count;
+}
+
+
+if ( $hyper_options['cache'] ) {
+	// Posts
+	add_action('publish_post', 'hyper_cache_invalidate', 0);
+	add_action('edit_post', 'hyper_cache_invalidate', 0);
+	add_action('delete_post', 'hyper_cache_invalidate', 0);
+	add_action('publish_phone', 'hyper_cache_invalidate', 0);
+	
+	// Coment ID is received
+	add_action('trackback_post', 'hyper_cache_invalidate', 0);
+	add_action('pingback_post', 'hyper_cache_invalidate', 0);
+	add_action('comment_post', 'hyper_cache_invalidate', 0);
+	add_action('edit_comment', 'hyper_cache_invalidate', 0);
+	add_action('wp_set_comment_status', 'hyper_cache_invalidate', 0);
+	
+	// No post_id is available
+	add_action('delete_comment', 'hyper_cache_invalidate', 0);
+	add_action('switch_theme', 'hyper_cache_invalidate', 0);
+}
 ?>
