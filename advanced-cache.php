211624396c75c9ef1338af63746196256685d286
satollo <satollo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-04-14 13:13:21 +0000

    git-svn-id: https://plugins.svn.wordpress.org/hyper-cache/trunk@40620 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- advanced-cache.php
+++ advanced-cache.php
@@ -1,44 +1,58 @@
 <?php
-@include(dirname(__FILE__) . '/hyper-cache-config.php');
+@include( dirname(__FILE__) . '/hyper-cache-config.php' );
 
 // From the config file
-if (!$hyper_cache_enabled) return;
-
-// Do not cache post request (comments, plugins and so on)
-if ($_SERVER["REQUEST_METHOD"] == 'POST') return;
-
-// Do not use or cache pages when a wordpress user is logged on
-foreach ($_COOKIE as $n=>$v)
-{
-    if (substr($n, 0, 13) == 'wordpressuser') return;
+if (!$hyper_cache_enabled) {
+	return false;
 }
 
-$hyper_uri = $_SERVER['REQUEST_URI'];
+// Do not cache post request (comments, plugins and so on)
+if ($_SERVER["REQUEST_METHOD"] == 'POST') {
+	return false;
+}
+
+// Do not use or cache pages when a wordpress user is logged on
+foreach ( (array) $_COOKIE as $n => $v ) {
+    // wp 2.5 and wp 2.3 have different cookie prefix
+    if (substr($n, 0, 14) == 'wordpressuser_' || substr($n, 0, 10) == 'wordpress_' ) {
+    	return false;
+    }
+}
+
+$hyper_uri = stripslashes($_SERVER['REQUEST_URI']);
 
 // Do not cache WP pages, even if those calls typically don't go throught this script
-if (strpos($hyper_uri, '?') !== false) return;
-if (strpos($hyper_uri, '/wp-admin/') !== false) return;
-if (strpos($hyper_uri, '/wp-includes/') !== false) return;
-if (strpos($hyper_uri, '/wp-content/') !== false) return;
+if( strpos($hyper_uri, '?') !== false ||
+	strpos($hyper_uri, '/wp-admin/') !== false ||
+	strpos($hyper_uri, '/wp-includes/') !== false ||
+	strpos($hyper_uri, '/wp-content/') !== false ) {
+	return false;
+}
+
 
 // Special blocks: the download manager plugin
-if (strpos($hyper_uri, '/download/') !== false) return;
+if (strpos($hyper_uri, '/download/') !== false) {
+	return false;
+}
 
 // Remove the anchor
 $x = strpos($hyper_uri, '#');
-if ($x !== false) $hyper_uri = substr($hyper_uri, 0, $x);
-
-
+if ($x !== false) {
+	$hyper_uri = substr($hyper_uri, 0, $x);
+}
 
 $hyper_cache_name = md5($hyper_uri);
 $hyper_file = ABSPATH . 'wp-content/hyper-cache/' . $hyper_cache_name . '.dat';
 
-if (file_exists($hyper_file))
-{
-    $hyper_data = unserialize(file_get_contents($hyper_file));
-    if ($hyper_cache_timeout == null) $hyper_cache_timeout = 60;
-    if ($hyper_data != null && ($hyper_data['time'] > time()-($hyper_cache_timeout*60)) && $hyper_data['html'] != '')
-    {
+if ( is_file($hyper_file) ) {
+    $hyper_data = unserialize( file_get_contents($hyper_file) );
+    
+    // Default timeout
+    if ($hyper_cache_timeout == null) {
+    	$hyper_cache_timeout = 60;
+    }
+    
+    if ($hyper_data != null && ($hyper_data['time'] > time()-($hyper_cache_timeout*60)) && $hyper_data['html'] != '') {
         header('Content-Type: text/html;charset=UTF-8');
         echo $hyper_data['html'];
         echo '<!-- hyper cache -->';
@@ -47,18 +61,16 @@ if (file_exists($hyper_file))
     }
 }
 
-//var_dump($_COOKIE);
 // Now we start the caching, but we remove the cookie which stores the comment for data
-foreach ($_COOKIE as $n=>$v)
-{
-    if (substr($n, 0, 14) == 'comment_author') unset($_COOKIE[$n]);
+foreach ( (array) $_COOKIE as $n => $v ) {
+    if (substr($n, 0, 14) == 'comment_author') {
+    	unset($_COOKIE[$n]);
+    }
 }
+
 ob_start('hyper_cache_callback');
-
-
 function hyper_cache_callback($buffer) {
     global $hyper_file;
-    //$name = md5($_SERVER['REQUEST_URI']);
 
     $data['uri'] = $_SERVER['REQUEST_URI'];
     $data['referer'] = $_SERVER['HTTP_REFERER'];
@@ -71,5 +83,4 @@ function hyper_cache_callback($buffer) {
 
     return $buffer;
 }
-
 ?>
--- readme.txt
+++ readme.txt
@@ -2,7 +2,7 @@
 Tags: cache,chaching
 Requires at least: 2.1
 Tested up to: 2.5
-Stable tag: 1.0.3
+Stable tag: 1.0.5
 Donate link: http://www.satollo.com/english/donate
 Contributors: satollo,momo360modena
 
