ee931016d59c28c0a79145dbd935be2ddb32128e
satollo <satollo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-10-16 20:43:59 +0000

    git-svn-id: https://plugins.svn.wordpress.org/hyper-cache/trunk@300738 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- plugin.php
+++ plugin.php
@@ -11,7 +11,7 @@ Disclaimer: Use at your own risk. No warranty expressed or implied is provided.
 ---
 Copyright 2008  Satollo  (email : info@satollo.net)
 ---
-
+ 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation; either version 2 of the License, or
